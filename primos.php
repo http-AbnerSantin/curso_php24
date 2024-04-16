c46cc3fade52483dcdd5649a1c87106c73628d94
Ábner Santin <93490804+http-AbnerSantin@users.noreply.github.com>
2024-04-16 20:54:05 -0300

    Update primos.php

--- primos.php
+++ primos.php
@@ -1,8 +1,20 @@
 <?php
-//Calcular os 5 primeiros numeros primos;
 
-    $numeros = [1,2,3,4,5,6,7,8,9,10,11];
+$numeros = [1,2,3,4,5,6,7,8,9,10,11];
 
-    
 
-?>
+for($i = 1; $i <= count($numeros); $i++) {
+    $divisores = 0;
+     
+    for($j = $i; $j >= 1; $j--)
+    {
+        if (($i % $j) == 0) {
+            $divisores++;
+        }
+    }
+     
+    if ($divisores == 2)
+    {
+        echo $i . '<br>';
+    }
+}
